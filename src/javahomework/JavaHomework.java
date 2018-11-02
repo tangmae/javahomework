5d83897d3f39b093b558a6fb5d82bc026aa96669
Suppassara K <suppassara.k@rakuten.com>
2018-11-02 09:48:40 +0900

    init

---
+++ src/javahomework/JavaHomework.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright   Rakuten, Inc. All Rights Reserved.
+ *
+ * This program is the information assets which are handled
+ * as "Strictly Confidential".
+ * Permission of Use is only admitted in Rakuten Inc.
+ * Development Department.
+ * If you don't have permission , MUST not be published,
+ * broadcast, rewritten for broadcast or publication
+ * or redistributed directly or indirectly in any medium.
+ */
+package javahomework;
+
+/**
+ * @author suppassara.k
+ *
+ */
+public class JavaHomework {
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("Hello World!");
+    }
+    
+}
