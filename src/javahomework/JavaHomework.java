f76c2ec16d8960b1b31c615556a474d052f2678e
Suppassara K <suppassara.k@rakuten.com>
2018-11-12 09:52:10 +0900

    updated homeworks

--- src/javahomework/JavaHomework.java
+++ src/javahomework/JavaHomework.java
@@ -16,13 +16,28 @@ package javahomework;
  *
  */
 public class JavaHomework {
-    
     /**
      * @param args
      */
+     // Create method to operate the number as the following
+     // plusNumber(int x, int y)
+     // minusNumber(int x, int y)
+     // multiplyNumber(int x, int y)
+     // divideNumber(int x, int y)  => x/y
+     // plusRangeOfNumber(int x, int y) => result of accumulating from x to y  => x + (x+1) + (x+2) + ... + y
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-        System.out.println("Hello World!");
+
     }
-    
+
+    // the method should return the number of result
+    // int result = plusNumber(1, 2);
+    // System.out.println(result); => output in the console must showing "3"
+    // https://www.safaribooksonline.com/videos/java-for-beginners/9781788996518/9781788996518-video3_11
+    // https://www.tutorialspoint.com/java/java_methods.htm
+    // or search "java method with return" on google
+    // the below method still incomplete and something incorrect please fix it ~!
+    public static void plusNumber(int x, int y) {
+
+    }
+
 }
